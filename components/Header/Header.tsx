f7d90dd01429371144b82d38a22e2e15623ca9be
Naman151 <namanshahi151@gmail.com>
2025-05-21 15:49:06 +0530

    Header Completed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,8 +4,14 @@ import Image from 'next/image'
 
 export default function() {
   return (
-    <div className='bg-[#F3F0EC] shadow-sm p-2'>
-       <div className='items-center justify-center'>
+    <div className='bg-[#F3F0EC] shadow-sm p-3 flex items-center justify-end'>
+       <div className='w-[400px] bg-white flex p-2 mx-5 rounded-md'>
+                <Image src={'/Search.svg'} alt='langicon' width={16} height={16}/>
+                <p className='text-[14px] font-[400] text-[#737373] ml-12'>Search for Books</p>
+       </div>
+
+
+       <div className='flex items-center mx-10'>
         <Button variant='outline' className='border-[#2C5A5E]'>
             <Image src={'/lang.svg'} alt='langicon' width={16} height={16}/>
             <p className='text-[14px] font-[400] text-black'>English</p>
